5a65599b4214e99ed5244ff8f17f0eb182035410
kinya ruth <45145197+kinyaruthm@users.noreply.github.com>
2021-07-20 11:52:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include "heading.php";
+//include "Login form.php";
+include "config1.php";
+//include " includes1/dashboard.php";
+//include "registration.php";
+?>
+<div id="content">
+	<div class="titledbackground">
+		<img class="img" src="images/taita.jpg">
+	</div>
+<p style="text-align:center;color:red;font-size: 35px;" ><strong> VOTE VOTE</strong></p>
+	</div>
+	<th height="41" colspan="2" scope="col"><h1><center> To start voting <a href="Login form.php">Click Here!!</center> </h1>
+		<hr>
+
+
+<?php
+	
+include "footers.php";
+		
+	?>
