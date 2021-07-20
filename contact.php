e5dc50a2f8273ed159101b6a62811d9898b684a3
kinya ruth <45145197+kinyaruthm@users.noreply.github.com>
2021-07-20 12:01:57 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,15 @@
+
+<?php
+//include"heading1.php"
+?>
+
+<div id="content">
+	<p align="center",color:#894ID;><strong>GJMRSW<br>
+	<i class="fafa phone"></i> Phone:+254746936220<br>
+	Email: gjmrsw@gmail.com</strong></p>
+	<hr>
+</div>
+<?php
+include "footers.php"
+?>
+	
